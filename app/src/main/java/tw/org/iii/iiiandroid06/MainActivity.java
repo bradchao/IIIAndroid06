d00026d30725e514ea7103e7d2c8dbbe119bec40
brad <brad@brad.tw>
2019-11-12 13:50:12 +0800

    4

--- app/src/main/java/tw/org/iii/iiiandroid06/MainActivity.java
+++ app/src/main/java/tw/org/iii/iiiandroid06/MainActivity.java
@@ -3,16 +3,23 @@ package tw.org.iii.iiiandroid06;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
     private TextView clock;
     private ListView lapList;
     private boolean isRunning;
     private Button btnLeft, btnRight;
+
+    private Timer timer;
+    private int i;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,13 +33,31 @@ public class MainActivity extends AppCompatActivity {
 
         btnLeft.setText(isRunning?"Lap":"Reset");
         btnRight.setText(isRunning?"Stop":"Start");
+
+        Log.v("brad", "start");
+        timer = new Timer();
     }
 
+    private class MyTask extends TimerTask {
+        @Override
+        public void run() {
+            i++;
+            Log.v("brad", "i = " + i);
+            clock.setText("" + i);
+        }
+    }
+
+
     public void clickLeft(View view) {
+
+
     }
     public void clickRight(View view) {
         isRunning = !isRunning;
         btnLeft.setText(isRunning?"Lap":"Reset");
         btnRight.setText(isRunning?"Stop":"Start");
+
+        timer.schedule(new MyTask(), 0, 10);
+
     }
 }
