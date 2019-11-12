23e95051caad347c709037cdcb7b66a60a671a75
brad <brad@brad.tw>
2019-11-12 11:53:33 +0800

    3

--- app/src/main/java/tw/org/iii/iiiandroid06/MainActivity.java
+++ app/src/main/java/tw/org/iii/iiiandroid06/MainActivity.java
@@ -4,17 +4,35 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView clock;
+    private ListView lapList;
+    private boolean isRunning;
+    private Button btnLeft, btnRight;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        clock = findViewById(R.id.clock);
+        lapList = findViewById(R.id.lapList);
+        btnLeft = findViewById(R.id.btnLeft);
+        btnRight = findViewById(R.id.btnRight);
+
+        btnLeft.setText(isRunning?"Lap":"Reset");
+        btnRight.setText(isRunning?"Stop":"Start");
     }
 
     public void clickLeft(View view) {
     }
     public void clickRight(View view) {
+        isRunning = !isRunning;
+        btnLeft.setText(isRunning?"Lap":"Reset");
+        btnRight.setText(isRunning?"Stop":"Start");
     }
 }
